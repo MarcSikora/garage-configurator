f42b98af65c2dab41d5749de6e956d6a4760f4dd
Marcin Sikora <memor37@onet.pl>
2021-11-16 23:22:14 +0100

    add handleChange()

--- src/compontents/inputs/SelectInput.js
+++ src/compontents/inputs/SelectInput.js
@@ -6,10 +6,14 @@ function SelectInput(props) {
         <option key={opt.value} value={opt.value}>{opt.text}</option>
     );
 
+    const handleChange = (e) => {
+        props.onChange(e.currentTarget.value);
+    }
+
     return (
         <div className="SelectInput">
             <label className="SelectInput--label">{props.label}</label>
-            <select className="SelectInput--select">
+            <select className="SelectInput--select" onChange={handleChange}>
                 {items}
             </select>
             <span className="SelectInput--unit">{props.unit}</span>
